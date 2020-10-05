5072c225eff49f0e3c59af4d731f51ec0db4a11d
dnovaes <novaesdigo@gmail.com>
2020-10-04 21:42:18 -0300

    Rename saveMovies payload

--- app/src/main/java/com/dnovaes/marvelmoviesredukt/models/payloads/SaveMoviesPayload.kt
+++ app/src/main/java/com/dnovaes/marvelmoviesredukt/models/payloads/SaveMoviesPayload.kt
@@ -2,7 +2,7 @@ package com.dnovaes.marvelmoviesredukt.models.payloads
 
 import com.dnovaes.marvelmoviesredukt.models.Movie
 
-data class SaveMoviePayload (
+data class SaveMoviesPayload (
     val sagaType: String,
     val content: List<Movie>
 )
--- app/src/main/java/com/dnovaes/marvelmoviesredukt/ui/anvil/HighOrderDSL.kt
+++ app/src/main/java/com/dnovaes/marvelmoviesredukt/ui/anvil/HighOrderDSL.kt
@@ -4,11 +4,6 @@ import android.view.View
 import trikita.anvil.Anvil
 import trikita.anvil.DSL
 
-/*
-* A collection of functions using high order functions feature from kotlin
-* to DRY UI code and give a elegant syntax to our custom components.
- */
-
 inline fun <reified T : View> highOrderComponent(crossinline func: T.() -> Unit) {
     DSL.v(T::class.java) {
         val block : T = Anvil.currentView()
